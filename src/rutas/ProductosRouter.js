36acb0c1e137c4fe9462dbf0435f2a652e6dfedc
Sergio Daniel Galván <sdg@reportsystem.com.ar>
2026-04-21 15:08:57 -0300

    Agregando router

---
+++ src/rutas/ProductosRouter.js
@@ -0,0 +1,32 @@
+// src/rutas/ProductosRouter.js
+import { Router } from "express";
+
+const router = Router();
+
+import {
+  getAllProductos,
+  getProductoById,
+  getProductoByNombre,
+  getAllProductosByCategoria,
+  getAllProductosWithStock,
+  createProducto,
+  deleteProductoById,
+  updateProductoWithStock,
+  updateProductoWithPrecio  
+} from "../controladores/ProductosControlador.js";
+
+// Rutas de productos Públicas
+router.get("/productos", getAllProductos);
+router.get("/productos/:id", getProductoById);
+router.get("/productos/nombre/:nombre", getProductoByNombre);
+router.get("/productos/categoria", getAllProductosByCategoria);
+
+// Rutas de productos Privadas
+router.get("/productos/stock", getAllProductosWithStock);
+router.post("/productos", createProducto);
+// Privada y Admin
+router.delete("/productos/:id", deleteProductoById);
+router.put("/productos/stock/:id", updateProductoWithStock);
+router.put("/productos/precio/:id", updateProductoWithPrecio);
+
+export default router;
