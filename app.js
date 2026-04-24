42b0a0af34a1e62ffa9ffd00c895420ded4e81b1
Lautaro Colella <lautarocolella1@gmail.com>
2026-04-24 17:42:08 -0300

    vistas de maestroProductos en pug

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // app.js
 import cors from "cors";
 import express from "express";
+import methodOverride from "method-override";
 import path from "path";
 import { fileURLToPath } from "url";
 import maestroProductosRouter from "./src/rutas/MaestroProductosRouter.js";
+import maestroProductosRouterViews from "./src/rutas/MaestroProductosRouterViews.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,25 +15,27 @@ const app = express();
 app.use(cors());
 
 // Configuración de Pug
-// app.set("view engine", "pug");
-// app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "src/vistas"));
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 // Rutas (se irán agregando)
 app.get("/", (req, res) => {
-  res.json({ titulo: "TodoStock S.A." });
+  res.render("index", { titulo: "TodoStock S.A." });
 });
 
 // app.use("/productos", productosRouter);
-app.use("/maestroproductos", maestroProductosRouter);
+app.use("/api/maestroproductos", maestroProductosRouter);
+app.use("/maestroproductos", maestroProductosRouterViews);
 
 // Manejo de errores 404
 app.use((req, res) => {
-  res.status(404).json({ titulo: "Página no encontrada" });
+  res.status(404).render("404", { titulo: "Página no encontrada" });
 });
 
 const PORT = process.env.PORT || 3000;
